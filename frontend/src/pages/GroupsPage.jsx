a2ea20974c2abd0f389cafcc2768a58e7012fa4b
uq88acaw <norul.gofran@fau.de>
2024-12-11 20:37:18 +0100

    Refactor GroupsPage to include AppBar and improved layout with Material-UI components

--- frontend/src/pages/GroupsPage.jsx
+++ frontend/src/pages/GroupsPage.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import Header from '../components/Header';
+import { Container, Typography, Box, AppBar, Toolbar, Button } from '@mui/material';
 import GroupList from '../components/GroupList';
 
 function GroupsPage() {
-  // Could add a form to create a new group
   return (
-    <div>
-      <Header />
-      <GroupList />
-    </div>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            ExpenseMate
+          </Typography>
+          {/* In a real app, check for authentication and show user's name or logout button */}
+          <Button color="inherit">Logout</Button>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="md" sx={{ mt: 4 }}>
+        <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
+          <Typography variant="h4" component="h1">Your Groups</Typography>
+          {/* A button to create a new group, if desired */}
+          <Button variant="contained" color="primary">Create Group</Button>
+        </Box>
+        <GroupList />
+      </Container>
+    </>
   );
 }
 
