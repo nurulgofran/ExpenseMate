ab01bd7dd595edbc49e528e970773bb7b8ff477e
uq88acaw <norul.gofran@fau.de>
2024-12-11 20:37:11 +0100

    Add authentication checks to routing and include login/register pages

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,14 +3,20 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import GroupsPage from './pages/GroupsPage';
 import ExpensesPage from './pages/ExpensesPage';
 import SettlementPage from './pages/SettlementPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 
 function AppRoutes() {
+  const token = localStorage.getItem('token');
+
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/groups" />} />
-      <Route path="/groups" element={<GroupsPage />} />
-      <Route path="/groups/:groupId/expenses" element={<ExpensesPage />} />
-      <Route path="/groups/:groupId/settlement" element={<SettlementPage />} />
+      <Route path="/" element={token ? <Navigate to="/groups" /> : <Navigate to="/login" />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/groups" element={token ? <GroupsPage /> : <Navigate to="/login" />} />
+      <Route path="/groups/:groupId/expenses" element={token ? <ExpensesPage /> : <Navigate to="/login" />} />
+      <Route path="/groups/:groupId/settlement" element={token ? <SettlementPage /> : <Navigate to="/login" />} />
     </Routes>
   );
 }
