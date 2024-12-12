bec91f3fe96d988fd2c7679ff948a95e27f70738
uq88acaw <norul.gofran@fau.de>
2024-12-12 19:05:58 +0100

    Refactor SettlementPage component to use Material-UI for improved layout

--- frontend/src/pages/SettlementPage.jsx
+++ frontend/src/pages/SettlementPage.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
 import SettlementSummary from '../components/SettlementSummary';
+import { Container, Typography } from '@mui/material';
 
 function SettlementPage() {
   const { groupId } = useParams();
 
   return (
-    <div>
+    <>
       <Header />
-      <h1>Settlement for Group {groupId}</h1>
-      <SettlementSummary groupId={groupId} />
-    </div>
+      <Container maxWidth="md" sx={{ mt: 4 }}>
+        <Typography variant="h4" mb={2}>Settlement for Group {groupId}</Typography>
+        <SettlementSummary groupId={groupId} />
+      </Container>
+    </>
   );
 }
 
