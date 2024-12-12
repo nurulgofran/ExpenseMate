eeda09ba92f988fd2d49eaaa25a235fd5ccfb784
uq88acaw <norul.gofran@fau.de>
2024-12-12 19:05:47 +0100

    Refactor GroupsPage component to use Header for improved layout and navigation

--- frontend/src/pages/GroupsPage.jsx
+++ frontend/src/pages/GroupsPage.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import { Container, Typography, Box, AppBar, Toolbar, Button } from '@mui/material';
+import { Container, Typography, Box, Button } from '@mui/material';
+import Header from '../components/Header';
 import GroupList from '../components/GroupList';
 
 function GroupsPage() {
   return (
     <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            ExpenseMate
-          </Typography>
-          {/* In a real app, check for authentication and show user's name or logout button */}
-          <Button color="inherit">Logout</Button>
-        </Toolbar>
-      </AppBar>
+      <Header />
       <Container maxWidth="md" sx={{ mt: 4 }}>
         <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
           <Typography variant="h4" component="h1">Your Groups</Typography>
-          {/* A button to create a new group, if desired */}
+          {/* Add group creation feature in future */}
           <Button variant="contained" color="primary">Create Group</Button>
         </Box>
         <GroupList />
